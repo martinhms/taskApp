7636c548cf2499c2b17dd29c15b89d38970a3301
Martin Horacio Mendez <martinhmendez@MacBook-Pro-de-Martin.local>
2024-07-10 18:15:41 -0300

    [TASK-SCREEN] - Added create and selected function

--- app/src/main/java/com/example/taskapp/ui/TaskScreen.kt
+++ app/src/main/java/com/example/taskapp/ui/TaskScreen.kt
@@ -1,16 +1,22 @@
 package com.example.taskapp.ui
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
@@ -28,18 +34,56 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
+import com.example.taskapp.ui.model.TaskModel
 
 @Composable
 fun TaskScreen(taskViewModel: TaskViewModel) {
 
     val showDialog: Boolean by taskViewModel.showDialog.observeAsState(initial = false)
     Box(modifier = Modifier.fillMaxSize()) {
-        AddTaskDialog(showDialog = showDialog, onDismiss = { taskViewModel.onDialogClose() }, onTaskAdded = {taskViewModel.onTaskCreated(it)})
+        AddTaskDialog(
+            showDialog = showDialog,
+            onDismiss = { taskViewModel.onDialogClose() },
+            onTaskAdded = { taskViewModel.onTaskCreated(it) })
         FloatButton(
             Modifier
                 .align(Alignment.BottomEnd)
-                .padding(16.dp)
-        ,taskViewModel)
+                .padding(16.dp), taskViewModel
+        )
+        TaskList(taskViewModel)
+    }
+
+}
+
+@Composable
+fun TaskList(taskViewModel: TaskViewModel) {
+    val taskList: List<TaskModel> = taskViewModel.tasks
+
+    LazyColumn {
+        //el parametro key sirve para optimizar el RV
+        items(taskList, key = { it.id }) { task ->
+            ItemTask(task = task, taskViewModel = taskViewModel)
+        }
+    }
+}
+
+@Composable
+fun ItemTask(task: TaskModel, taskViewModel: TaskViewModel) {
+    Card(
+        modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
+        border = BorderStroke(2.dp, Color.Blue)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.White),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = task.task, modifier = Modifier.weight(1f))
+            Checkbox(
+                checked = task.selected,
+                onCheckedChange = { taskViewModel.onCheckBoxSelected(task) })
+        }
     }
 
 }
@@ -47,7 +91,7 @@ fun TaskScreen(taskViewModel: TaskViewModel) {
 @Composable
 fun FloatButton(padding: Modifier, taskViewModel: TaskViewModel) {
     FloatingActionButton(
-        onClick = {taskViewModel.onDialogOpen()},
+        onClick = { taskViewModel.onDialogOpen() },
         modifier = padding
     ) {
         Icon(Icons.Filled.Add, contentDescription = "")
@@ -78,7 +122,10 @@ fun AddTaskDialog(showDialog: Boolean, onDismiss: () -> Unit, onTaskAdded: (Stri
                     maxLines = 1
                 )
                 Spacer(modifier = Modifier.size(16.dp))
-                Button(onClick = { onTaskAdded(task) }, modifier = Modifier.fillMaxWidth()) {
+                Button(onClick = {
+                    onTaskAdded(task)
+                    task = ""
+                }, modifier = Modifier.fillMaxWidth()) {
                     Text(text = "Add task")
                 }
             }
--- app/src/main/java/com/example/taskapp/ui/TaskViewModel.kt
+++ app/src/main/java/com/example/taskapp/ui/TaskViewModel.kt
@@ -1,9 +1,11 @@
 package com.example.taskapp.ui
 
 import android.util.Log
+import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.taskapp.ui.model.TaskModel
 import javax.inject.Inject
 
 
@@ -12,17 +14,26 @@ class TaskViewModel @Inject constructor() : ViewModel() {
     private val _showDialog = MutableLiveData<Boolean>()
     val showDialog: LiveData<Boolean> = _showDialog
 
+    private val _tasks = mutableStateListOf<TaskModel>()
+    val tasks: List<TaskModel> = _tasks
 
     fun onDialogClose() {
         _showDialog.value = false
     }
 
     fun onTaskCreated(task: String) {
-            Log.i("task",task)
+        _tasks.add(TaskModel(task = task))
         _showDialog.value = false
     }
 
     fun onDialogOpen() {
         _showDialog.value = true
+    }
+
+    fun onCheckBoxSelected(task: TaskModel) {
+        val index = _tasks.indexOf(task)
+        _tasks[index] = _tasks[index].let {
+            it.copy(selected = !it.selected)
+        }
     }
 }
