5e32029d006e341fd219393158f30d358c60ae0b
martinhms <martinhms2015@gmail.com>
2024-07-10 14:50:18 -0300

    [Init-configs] - Initial configs and dependencies (#1)
    * [Init-configs] - Initial configs and dependencies
    
    * [TASK-SCREEN] - Added UI taskScreen
    
    ---------
    
    Co-authored-by: Martin Horacio Mendez <martinhmendez@MBP-de-Martin.fibertel.com.ar>

---
+++ app/src/main/java/com/example/taskapp/ui/TaskScreen.kt
@@ -0,0 +1,87 @@
+package com.example.taskapp.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+
+@Composable
+fun TaskScreen(taskViewModel: TaskViewModel) {
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        AddTaskDialog(show = true, onDismiss = {}, onTaskAdded = {})
+        FloatButton(
+            Modifier
+                .align(Alignment.BottomEnd)
+                .padding(16.dp)
+        )
+    }
+
+}
+
+@Composable
+fun FloatButton(padding: Modifier) {
+    FloatingActionButton(
+        onClick = {},
+        modifier = padding
+    ) {
+        Icon(Icons.Filled.Add, contentDescription = "")
+    }
+}
+
+
+@Composable
+fun AddTaskDialog(show: Boolean, onDismiss: () -> Unit, onTaskAdded: (String) -> Unit) {
+    var task by remember { mutableStateOf("") }
+    if (show) {
+        Dialog(onDismissRequest = { onDismiss() }) {
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .background(Color.White)
+                    .padding(16.dp)
+            ) {
+                Text(
+                    text = "Add task",
+                    fontSize = 16.sp,
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    fontWeight = FontWeight.Bold,
+                )
+                Spacer(modifier = Modifier.size(16.dp))
+                TextField(
+                    value = task, onValueChange = { task = it }, singleLine = true,
+                    maxLines = 1
+                )
+                Spacer(modifier = Modifier.size(16.dp))
+                Button(onClick = { onTaskAdded(task) }, modifier = Modifier.fillMaxWidth()) {
+                    Text(text = "Add task")
+                }
+            }
+
+        }
+    }
+
+}
